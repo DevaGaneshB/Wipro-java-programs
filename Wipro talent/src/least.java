04674e059458ce2fb76e093733ec70dc76466f4a
DevaGaneshB <40600219+DevaGaneshB@users.noreply.github.com>
2018-08-04 18:52:56 +0530

    Add files via upload

--- Wipro talent/src/least.java
+++ Wipro talent/src/least.java
@@ -1,17 +1,24 @@
-
+import java.util.Scanner;
 public class least {
-void less(int arr[]) {
-	int less=arr[0];
-	for(int i=0;i<arr.length;i++)
-	{
-		less=(arr[i]>arr[i+1])?arr[i+1]:arr[i];
+void findleast(int arr[]) {
+	int min = arr[0];
+	for (int i=0;i<arr.length;i++) {
+		if(min>arr[i])
+			min=arr[i];
 	}
-	if(less>0) {System.out.println("Least="+(less-1));}
-	else {System.out.println("Least="+0);}
-}
+	if(min==0)
+		System.out.println(min);
+	else
+		System.out.println(min-1);
+	}
 public static void main(String args[]) {
-	least num=new least();
-	int a[]= {1,23,4,5,2};
-	num.less(a);
+	System.out.println("Enter 6 numbers");
+	Scanner sc=new Scanner(System.in);
+	int [] arr= new int[6];
+	for (int i=0;i<arr.length;i++) {
+		arr[i]=sc.nextInt();
+	}
+	least a=new least();
+	a.findleast(arr);
 }
 }
